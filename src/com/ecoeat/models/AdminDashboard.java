40167c14419c5ad67d1bd0fee223565d8c42599c
malek <malekkhaloui453@gmail.com>
2026-04-26 19:43:26 +0100

    creation de AdminDashboard

---
+++ src/com/ecoeat/models/AdminDashboard.java
@@ -0,0 +1,13 @@
+package com.ecoeat.models;
+
+public class AdminDashboard {
+
+    public void afficherStatistiques() {
+        // TODO: Implement afficherStatistiques
+    }
+
+    public void validerRepas() {
+        // TODO: Implement validerRepas
+    }
+
+}
